5b64e7eafdb57631b53bf1551d5bf80e6f0f4d6c
timen.stepisnik.perdih@in516ht.com <timen.stepisnik.perdih@in516ht.com>
2022-07-05 11:39:02 +0200

    individual practice template

--- Project/demo-app/src/app/components/stream/stream.component.ts
+++ Project/demo-app/src/app/components/stream/stream.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, HostListener } from '@angular/core';
 import { Subscription, Observable, fromEvent } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 import { StreamService } from 'src/app/services/stream.service';
@@ -10,11 +10,31 @@ import 'rxjs/add/observable/fromEvent';
   styleUrls: ['./stream.component.css']
 })
 export class StreamComponent implements OnInit, OnDestroy {
+  @HostListener('window:resize', ['$event'])
+  onResize(event?) {
+     this.screenHeight = window.innerHeight;
+     this.screenWidth = window.innerWidth;
+
+      this.targetX = this.getRandomInt(this.screenWidth);
+      this.targetY = this.getRandomInt(this.screenHeight);
+
+      console.log("Target set to:", this.targetX, this.targetY);
+  }
+
+  private getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 
   constructor(
     private streamService: StreamService
-  ) { }
+  ) { 
+    this.onResize();
+  }
 
+    private screenHeight: number;
+    private screenWidth: number;
+    private targetX: number;
+    private targetY: number;
     private streamSubscription: Subscription;
 
   ngOnInit(): void {
@@ -27,7 +47,10 @@ export class StreamComponent implements OnInit, OnDestroy {
     //   (light) => console.log('command:', light)
     // )
 
-    fromEvent(document.body, 'mousemove').subscribe( (e: MouseEvent) => { //npm install --save rxjs-compat   da bo delalo :)
+    fromEvent(document.body, 'mousemove')
+    .pipe(
+
+    ).subscribe( (e: MouseEvent) => { //npm install --save rxjs-compat   da bo delalo :)
       console.log(e.pageX, e.pageY);
     })
   }
