408bd95f1567cfa37c999b99b5738d1f28dbe7f7
timen.stepisnik.perdih@in516ht.com <timen.stepisnik.perdih@in516ht.com>
2022-07-05 11:32:46 +0200

    mouse event template

--- Project/demo-app/src/app/components/stream/stream.component.ts
+++ Project/demo-app/src/app/components/stream/stream.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable, fromEvent } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 import { StreamService } from 'src/app/services/stream.service';
+import 'rxjs/add/observable/fromEvent';
 
 @Component({
   selector: 'app-stream',
@@ -17,14 +18,18 @@ export class StreamComponent implements OnInit, OnDestroy {
     private streamSubscription: Subscription;
 
   ngOnInit(): void {
-    this.streamSubscription = this.streamService.getStream()
-    .pipe(
-      //filter((light) => light === 'red' || light === 'yellow'),
-      map((light) => this.extractCommand(light))
-    )
-    .subscribe(
-      (light) => console.log('command:', light)
-    )
+    // this.streamSubscription = this.streamService.getStream()
+    // .pipe(
+    //   //filter((light) => light === 'red' || light === 'yellow'),
+    //   map((light) => this.extractCommand(light))
+    // )
+    // .subscribe(
+    //   (light) => console.log('command:', light)
+    // )
+
+    fromEvent(document.body, 'mousemove').subscribe( (e: MouseEvent) => { //npm install --save rxjs-compat   da bo delalo :)
+      console.log(e.pageX, e.pageY);
+    })
   }
 
   private extractCommand(light: string){
